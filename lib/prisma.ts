8d970c3ecd09d84e346ea70a14a3e5a454aa7b17
estevao-castilho <ecsa2@aluno.ifnmg.edu.br>
2025-07-07 20:59:56 +0000

    .

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,16 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client'
 
-let prisma: PrismaClient;
-
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
+// 1. Extensão de tipo para globalThis
+declare global {
+  var prisma: PrismaClient | undefined
 }
 
-export default prisma;
+// 2. Inicialização segura com TypeScript
+const prisma: PrismaClient = globalThis.prisma || new PrismaClient()
+
+// 3. Preservar a instância em desenvolvimento
+if (process.env.NODE_ENV !== 'production') {
+  globalThis.prisma = prisma
+}
+
+export default prisma
