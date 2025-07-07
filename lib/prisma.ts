f91a904147cb0c403c75129b9191148d792b1200
estevao-castilho <ecsa2@aluno.ifnmg.edu.br>
2025-07-07 20:54:21 +0000

    .

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,14 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 
-declare global {
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
 }
 
-const prisma = globalThis.prisma || new PrismaClient()
-
-if (process.env.NODE_ENV !== 'production') {
-  globalThis.prisma = prisma
-}
-
-export default prisma
+export default prisma;
