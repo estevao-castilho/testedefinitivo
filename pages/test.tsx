b9bebc1e1cfbdf21b69f9ae78ffe18cadb5070d9
estevao-castilho <ecsa2@aluno.ifnmg.edu.br>
2025-07-09 00:35:47 +0000

    123

---
+++ pages/test.tsx
@@ -0,0 +1,14 @@
+// pages/test.tsx
+import React from 'react';
+
+// Componente React padrão
+const TestPage = () => {
+  return (
+    <div>
+      <h1>Página de Teste</h1>
+      <p>Conteúdo da página</p>
+    </div>
+  );
+};
+
+export default TestPage; // Exportação padrão obrigatória
